5835e7fa9f2d58e96a6540ff4f1dae3c0f16b13a
prasadbabu1 <147583347+prasadbabu1@users.noreply.github.com>
2024-02-17 15:28:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let hr = min = sec = ms ="0" + 0 ,
+ startTimer;
+
+ const startBtn = document.querySelector(".start"),
+       stopBtn = document.querySelector(".stop"),
+       resetBtn = document.querySelector(".reset") 
+
+
+       startBtn.addEventListener("click",start);
+       stopBtn.addEventListener("click",stop);
+       resetBtn.addEventListener("click",reset);
